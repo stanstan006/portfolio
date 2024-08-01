0dffc33da290a8353c0a91dbb5a8bc4146918ece
ALADIN <aladin@iMac-de-ALADIN.local>
2024-08-01 10:11:49 +0000

    j'ai juste renommer les fichiers index.html  style.css javaScript.js

--- mnp.js
+++
@@ -1,9 +0,0 @@
-function rest(){ 
-let inputconte = document.getElementById("form-control").value
-let set =inputconte
-if(set){
-    localStorage.setItem("sud",set)
-    window.location.href=""
-    alert("merci a vous")
-}
-}
